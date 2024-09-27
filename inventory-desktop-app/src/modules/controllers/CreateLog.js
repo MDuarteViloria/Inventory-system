767189efbe49d67a55b22e74d8335e6558ebef1f
Sebastian Leañez <thesebalex111@gmail.com>
2024-09-27 00:30:38 -0400

    log date

--- inventory-desktop-app/src/modules/controllers/CreateLog.js
+++ inventory-desktop-app/src/modules/controllers/CreateLog.js
@@ -3,8 +3,8 @@ import DB from "../database/connect-db";
 export default async function CreateInventoryLog(Type) {
   const database = new DB();
 
-  const sql = "INSERT INTO InventoryLog (Type) VALUES (?)";
-  await database.query(sql, [Type]);
+  const sql = "INSERT INTO InventoryLog (Date, Type) VALUES (?, ?)";
+  await database.query(sql, [new Date().getTime(), Type]);
 
   database.close();
 }
